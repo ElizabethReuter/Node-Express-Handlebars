5c7256eb626b395bc4ad646c01ed37281379eef3
Elizabeth Reuter <elizabeth.j.reuter@gmail.com>
2020-11-02 09:07:31 -0600

    added more files

--- server.js
+++ server.js
@@ -1,29 +1,30 @@
-const express = require("express");
+//  dependencies
+const express = require('express');
+const exphbs = require('express-handlebars')
 
-const PORT = process.env.PORT || 8080;
+const db = require('./models');
 
+const routes = require('./routes');
+
+// create an instance of express
 const app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
+// either heroku or localhost
+const PORT = process.env.PORT || 8080;
+
 app.use(express.static("public"));
-
-// Parse application body as JSON
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-
-// Set Handlebars.
-var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-var routes = require("./controllers/burgers_Controller.js");
-
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
-});
+db.sequelize.sync().then(function() {
+    app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  })
+}); 
