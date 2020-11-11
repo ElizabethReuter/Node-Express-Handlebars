7f96c1ad46625c98c3ef85f78aef1578227d950d
Elizabeth Reuter <elizabeth.j.reuter@gmail.com>
2020-11-10 20:00:14 -0500

    fixed controller.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,28 +1,51 @@
- // communicates with db
+var express = require("express");
 
-const db = require('../models');
+var router = express.Router();
 
-module.exports = {
-//functionality of this controller
-    //Get all the burgers
-    smashAllBurgs: function(req, res) {   
-        db.Burger.find({}).then(function(dbBurgerData) {res.json(dbBurgerData)})
+// Import the model (Burger.js) to use its database functions.
+var burger = require("../models/Burger.js");
 
-    },
-    // // Get burger by id
-    // smashSpecificBurger: function(req, res) {
 
-    // },
-    // // Add burger
-    // addBurger: function(req, res) {
+// Create all our routes and set up logic within those routes where required
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+      var hbsObject = {
+        burgers: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
 
-    // },
-    // // DEL burger
-    // delBuger: function(req, res) {
 
-    // },
-    // // UPD  burger
-    // updBurger: function(req,res) {
+  router.post("/api/burgers", function(req, res) {
+    burger.create(
+        [
+        "burger_name", "devoured"
+    ], [
+      req.body.burger_name, req.body.devoured
+    ], function(result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    });
+  });
 
-    // }
-};
+  router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+    burger.update({
+      devoured: req.body.devoured
+    }, condition, function(result) {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
+
+
+module.exports = router;
