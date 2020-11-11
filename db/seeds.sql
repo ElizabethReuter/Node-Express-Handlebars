220def0718000f1865d21ffa219b4d2f7a638593
Elizabeth Reuter <elizabeth.j.reuter@gmail.com>
2020-11-10 20:03:13 -0500

    fixed schema and seed

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('California Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Ranch Burger', false);
